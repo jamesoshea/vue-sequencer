8daea1b1512ed0bffc2ac495106b8d19815cd2c2
James O'Shea <jo@3yourmind.com>
2018-05-05 15:11:18 +0200

    refactor: break patterns up into own files

---
+++ src/assets/patterns/index.js
@@ -0,0 +1,12 @@
+import samba from './samba.js';
+import house from './house.js';
+import ukGarage from './ukGarage.js';
+
+
+const patterns = {
+  samba,
+  house,
+  ukGarage,
+}
+
+export default patterns
